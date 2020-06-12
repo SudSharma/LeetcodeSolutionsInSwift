4cf30751518b8e3d221cc7d28eafcbb2beb09a08
Sudarshan Sharma <sudarshan.sharma@aol.in>
2020-06-12 12:37:53 -0700

    380. Insert Delete GetRandom O(1)

--- Leetcode/Leetcode/Questions/380.InsertDeleteGetRandomO(1).swift
+++ Leetcode/Leetcode/Questions/380.InsertDeleteGetRandomO(1).swift
@@ -6,4 +6,77 @@
 //  Copyright © 2020 Sudarshan Sharma. All rights reserved.
 //
 
-import Foundation
+/*
+ Design a data structure that supports all following operations in average O(1) time.
+
+ insert(val): Inserts an item val to the set if not already present.
+ remove(val): Removes an item val from the set if present.
+ getRandom: Returns a random element from current set of elements. Each element must have the same probability of being returned.
+ Example:
+
+ // Init an empty set.
+ RandomizedSet randomSet = new RandomizedSet();
+
+ // Inserts 1 to the set. Returns true as 1 was inserted successfully.
+ randomSet.insert(1);
+
+ // Returns false as 2 does not exist in the set.
+ randomSet.remove(2);
+
+ // Inserts 2 to the set, returns true. Set now contains [1,2].
+ randomSet.insert(2);
+
+ // getRandom should return either 1 or 2 randomly.
+ randomSet.getRandom();
+
+ // Removes 1 from the set, returns true. Set now contains [2].
+ randomSet.remove(1);
+
+ // 2 was already in the set, so return false.
+ randomSet.insert(2);
+
+ // Since 2 is the only number in the set, getRandom always return 2.
+ randomSet.getRandom();
+*/
+
+class RandomizedSet {
+    var dict: [Int: Int]
+    var storage: [Int]
+    
+    /** Initialize your data structure here. */
+    init() {
+        dict = [Int: Int]()
+        storage = [Int]()
+    }
+    
+    /** Inserts a value to the set. Returns true if the set did not already contain the specified element. */
+    func insert(_ val: Int) -> Bool {
+        if let _ = dict[val] {
+            return false
+        }
+        else {
+            storage.append(val)
+            dict[val] = storage.count - 1
+            return true
+        }
+    }
+    
+    /** Removes a value from the set. Returns true if the set contained the specified element. */
+    func remove(_ val: Int) -> Bool {
+        if dict.isEmpty || dict[val] == nil {
+            return false
+        }
+        storage[dict[val]!] = storage[storage.count - 1]
+        dict[storage[storage.count - 1]] = dict[val]!
+        storage.removeLast()
+        dict[val] = nil
+        
+        return true
+    }
+    
+    /** Get a random element from the set. */
+    func getRandom() -> Int {
+        let random = Int.random(in: 0..<storage.count)
+        return storage[random]
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,28 @@
-# LeetcodeSolutionsInSwift
-Leetcode Solutions In Swift
+# Leetcode Solutions In Swift
+
+380. Insert Delete GetRandom O(1)
+
+<details><summary>Approach</summary>
+<p>
+
+`insert(x)`
+1. Check if x is already present in dictionary.
+2. If not present, then insert it at the end of the array.
+3. Add in the dictionary, x is added as key and last array index as the value.
+
+`remove(x)`
+1. Return false if dictionary is empty or val(key) is not present in dictionary
+2. Get value for key from dictionary and swap element at that index with last index in array
+3. Update swapped element value with its index in dictionary
+4. Remove last element from array
+5. Remove val(key) from dictionary 
+
+`getRandom()`
+1. Generate a random number from 0 to last index.
+2. Return the array element at the randomly generated index.
+
+`search(x)`
+1. Do a lookup for x in dictionary.
+
+</p>
+</details>
